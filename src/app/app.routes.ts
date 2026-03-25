d78534f23be5fa28371a667d78c5f4f8d8b13d27
smchedlidze826 <saba.mchedlidze16@gmail.com>
2026-03-25 00:17:12 -0700

    shell

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+   {
+    path: 'remote',
+    loadChildren: () =>
+      loadRemoteModule('remote', './routes').then((m) => m.REMOTE_ROUTES),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('shell');
+  constructor(private readonly router: Router) {}
+
+  loadRemoteApp(): void {
+    this.router.navigate(['/remote']);
+  }
 }
